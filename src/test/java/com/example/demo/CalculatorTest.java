aeb30f179f59b654ee4869e548451d6dc4b7109b
Mateusz Miotk <mateusz.miotk@ug.edu.pl>
2024-06-14 09:03:48 +0200

    add tests

---
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testSum() {
+        assertEquals(5, calculator.sum(2,3));
+    }
+}
